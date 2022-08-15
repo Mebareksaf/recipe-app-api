c08d7360a86eeca181fef18c7aed35f384ff0779
MebarekSaf <mebareksafari8@gmail.com>
2022-08-15 13:08:08 +0100

    implements creating tag feature

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -249,7 +249,7 @@ class PrivateRecipeAPITests(TestCase):
         self.assertEqual(recipe.tags.count(), 2)
         self.assertIn(tag_indian, recipe.tags.all())
         for tag in payload['tags']:
-            exists = recipe.tag.filter(
+            exists = recipe.tags.filter(
                 name=tag['name'],
                 user=self.user
             ).exists()
